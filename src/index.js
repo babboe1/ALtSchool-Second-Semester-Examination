51bab60bfda8eca275bf55acf6ac3fae5ea6986a
babboeCodes <babboecodes@gmail.com>
2022-11-04 15:19:35 +0100

    feat: add axios interceptors - create default api url - add request and response interceptor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://api.github.com/users/babboe1';
+
+axios.interceptors.request.use(
+   (request) => {
+      return request;
+   },
+   (error) => {
+      console.log(error);
+      return Promise.reject(error);
+   },
+);
+
+axios.interceptors.response.use(
+   (response) => {
+      return response;
+   },
+   (error) => {
+      console.log(error);
+      return Promise.reject(error);
+   },
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <BrowserRouter>
+      <App />
+   </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
