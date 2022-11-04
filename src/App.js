8d14abb0345bdaec807bfc81d7b2c3129545fa4f
babboeCodes <babboecodes@gmail.com>
2022-11-04 15:16:17 +0100

    Update app component - fetch data with axios api - add routes to component - add pagination to repositories page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Context from './components/Context/Context';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
+import Overview from './components/Pages/Overview/Overview';
+import Header from './components/UI/Header/Header';
+import instance from './components/Axios/Axios';
+import PaginatedPages from './components/Paginate/PaginatedPages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+   const [userData, setUserData] = useState('');
+   const [ userRepos, setUserRepos ] = useState( '' );
+   const [errorMsg, setErrorMsg] = useState();
+   useEffect(() => {
+      axios
+         .get('')
+         .then((res) => {
+            setUserData(res);
+         })
+         .catch( ( error ) => {
+            setErrorMsg(error.message)
+            console.log(error.message);
+         });
+      axios
+         .get('/repos?per_page=100')
+         .then((res) => {
+            setUserRepos( res );
+         })
+         .catch( ( error ) => {
+            setErrorMsg(error.message);
+         });
+      instance
+         .get('/Loopstudio-Landing-Page')
+         .then((res) => {
+            // console.log(res);
+         })
+         .catch((error) => {
+            setErrorMsg(error.message);
+         });
+      instance
+         .get(
+            '/Loopstudio-Landing-Page/contents/README.md',
+         )
+         .then((res) => {
+            // console.log(res);
+         })
+         .catch((error) => {
+            setErrorMsg(error.message);
+         });
+   }, [] );
+   
+   const value = {
+      userData,
+      userRepos,
+      errorMsg
+   }
+   
+   return (
+      <Context.Provider value={value}>
+         <Header />
+         <main>
+            <Route path="/overview">
+               <Overview />
+            </Route>
+            <Route path="/repos">
+               <PaginatedPages itemsPerPage={6} />
+            </Route>
+         </main>
+      </Context.Provider>
+   );
+};
 
 export default App;
