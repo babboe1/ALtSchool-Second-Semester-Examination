afe0a0b30ee976f89182237e9a9acad6cbbf489a
babboeCodes <babboecodes@gmail.com>
2022-11-07 08:36:35 +0100

    update component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,86 @@
 import React, { useState, useEffect } from 'react';
 import Context from './components/Context/Context';
 import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Overview from './components/Pages/Overview/Overview';
 import Header from './components/UI/Header/Header';
-import instance from './components/Axios/Axios';
 import PaginatedPages from './components/Paginate/PaginatedPages';
+import Repo from './components/Pages/Repo/Repo';
+import Readme from './components/Pages/Repositories/RepoTile/ViewRepoBtn/Readme/Readme';
 
 const App = () => {
    const [userData, setUserData] = useState('');
-   const [ userRepos, setUserRepos ] = useState( '' );
-   const [errorMsg, setErrorMsg] = useState();
+   const [userRepos, setUserRepos] = useState('');
+   const [ errorMsg, setErrorMsg ] = useState();
+   const [ readme, setReadme ] = useState('');
+   
    useEffect(() => {
       axios
          .get('')
          .then((res) => {
             setUserData(res);
          })
-         .catch( ( error ) => {
-            setErrorMsg(error.message)
+         .catch((error) => {
+            setErrorMsg(error.message);
             console.log(error.message);
          });
       axios
          .get('/repos?per_page=100')
          .then((res) => {
-            setUserRepos( res );
-         })
-         .catch( ( error ) => {
-            setErrorMsg(error.message);
-         });
-      instance
-         .get('/Loopstudio-Landing-Page')
-         .then((res) => {
-            // console.log(res);
+            setUserRepos(res);
          })
          .catch((error) => {
             setErrorMsg(error.message);
          });
-      instance
-         .get(
-            '/Loopstudio-Landing-Page/contents/README.md',
-         )
-         .then((res) => {
-            // console.log(res);
-         })
-         .catch((error) => {
-            setErrorMsg(error.message);
-         });
-   }, [] );
-   
+      // instance
+      //    .get('/Loopstudio-Landing-Page')
+      //    .then((res) => {
+      //       // console.log(res);
+      //    })
+      //    .catch((error) => {
+      //       setErrorMsg(error.message);
+      //    });
+      // instance
+      //    .get('/Loopstudio-Landing-Page/contents/README.md')
+      //    .then((res) => {
+      //       // console.log(res);
+      //    })
+      //    .catch((error) => {
+      //       setErrorMsg(error.message);
+      //    });
+   }, []);
+
    const value = {
       userData,
       userRepos,
-      errorMsg
-   }
-   
+      errorMsg,
+      readme,
+      setReadme,
+   };
+
+   const routeData = userRepos ? userRepos.data : [];
+   const routeDataMap = routeData.map((item) => (
+      <Route
+         path={`repos/${item.name}`}
+         key={item.id}
+         element={ <Repo name={ item.name } about={ item.description } page={ item.homepage } url={ item.html_url} />}
+      >
+         <Route path="readme" element={ <Readme name={ item.name }/>} />
+      </Route>
+   ));
+
    return (
       <Context.Provider value={value}>
          <Header />
          <main>
-            <Route path="/overview">
-               <Overview />
-            </Route>
-            <Route path="/repos">
-               <PaginatedPages itemsPerPage={6} />
-            </Route>
+            <Routes>
+               <Route path="/" element={<Overview />} />
+               <Route
+                  path="repos"
+                  element={<PaginatedPages itemsPerPage={6} />}
+               />
+               {routeDataMap}
+            </Routes>
          </main>
       </Context.Provider>
    );
