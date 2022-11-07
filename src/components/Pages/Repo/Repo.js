57cf915c6d29e2bd60265f2a26a78140cf8d5d33
babboeCodes <babboecodes@gmail.com>
2022-11-07 08:37:43 +0100

    Create Repo - implement nested routes - fetch data with axios instance

---
+++ src/components/Pages/Repo/Repo.js
@@ -0,0 +1,45 @@
+import React, {useContext} from 'react';
+import Image from '../../UI/Image/Image';
+import RepoTile from '../Repositories/RepoTile/RepoTile';
+import ViewRepoBtn from '../Repositories/RepoTile/ViewRepoBtn/ViewRepoBtn';
+import classes from './Repo.module.css';
+import githubImage from '../../../assets/icons/github.svg';
+import RepoLinkBtn from '../Repositories/RepoTile/RepoLinkBtn/RepoLinkBtn';
+import Context from '../../Context/Context';
+
+const Repo = ( props ) => {
+   const context = useContext(Context);
+   return (
+      <div className={classes['repositories']}>
+         <RepoTile name={props.name} about={props.about}>
+            <ViewRepoBtn
+               sty="flip-card__action"
+               text="Live Link"
+               page={props.page}
+            />
+            <ViewRepoBtn
+               sty="project-tile__link"
+               image={
+                  <Image
+                     src={githubImage}
+                     radius="50%"
+                     maxWidth="30px"
+                     alt="github icon"
+                  />
+               }
+               text="Github"
+               page={props.url}
+            />
+            <RepoLinkBtn
+               port={props.port}
+               setPort={props.setPort}
+               act="README.md"
+               name="readme"
+            />
+         </RepoTile>
+         {context.readme}
+      </div>
+   );
+};
+
+export default Repo;
