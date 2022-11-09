c56c7e720d464044122d68e43bbe38ed1171f375
babboeCodes <babboecodes@gmail.com>
2022-11-09 20:18:41 +0100

    update component - add error boundary

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import PaginatedPages from './components/Paginate/PaginatedPages';
 import Repo from './components/Pages/Repo/Repo';
 import Readme from './components/Pages/Repositories/RepoTile/ViewRepoBtn/Readme/Readme';
 import Backdrop from './components/Modals/Backdrop/Backdrop';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorCounter from './components/Pages/Error/ErrorCounter/ErrorCounter';
+import Fallback from './components/Pages/Error/FallBack/Fallback';
 
 const App = () => {
    const [userData, setUserData] = useState('');
    const [userRepos, setUserRepos] = useState('');
-   const [ errorMsg, setErrorMsg ] = useState();
-   const [ readme, setReadme ] = useState('');
-   const [ menuModal, setMenuModal ] = useState(false);
-   
+   const [errorMsg, setErrorMsg] = useState();
+   const [readme, setReadme] = useState('');
+   const [menuModal, setMenuModal] = useState(false);
+   const [ count, setCount ] = useState( 2 );
+   const [ countError, setCountError ] = useState( null );
+
    useEffect(() => {
       axios
          .get('')
@@ -37,11 +42,16 @@ const App = () => {
    }, []);
 
    const showMenuModal = () => {
-      setMenuModal( !menuModal );
-   }
+      setMenuModal(!menuModal);
+   };
    const removeMenuModal = () => {
-      setMenuModal( false );
-   }
+      setMenuModal(false);
+   };
+
+   const onError = (error, errorInfo) => {
+      setCountError(error);
+      console.log(error, errorInfo);
+   };
 
    const value = {
       userData,
@@ -51,7 +61,11 @@ const App = () => {
       setReadme,
       menuModal,
       showMenuModal,
-      removeMenuModal
+      removeMenuModal,
+      count,
+      setCount,
+      countError,
+      setCountError,
    };
 
    const routeData = userRepos ? userRepos.data : [];
@@ -59,27 +73,37 @@ const App = () => {
       <Route
          path={`repos/${item.name}`}
          key={item.id}
-         element={ <Repo name={ item.name } about={ item.description } page={ item.homepage } url={ item.html_url} />}
+         element={
+            <Repo
+               name={item.name}
+               about={item.description}
+               page={item.homepage}
+               url={item.html_url}
+            />
+         }
       >
-         <Route path="readme" element={ <Readme name={ item.name }/>} />
+         <Route path="readme" element={<Readme name={item.name} />} />
       </Route>
    ));
 
    return (
-      <Context.Provider value={value}>
-         <Header />
-         <Backdrop click={()=>setMenuModal(false) } />
-         <main>
-            <Routes>
-               <Route path="/" element={<Overview />} />
-               <Route
-                  path="repos"
-                  element={<PaginatedPages itemsPerPage={6} />}
-               />
-               {routeDataMap}
-            </Routes>
-         </main>
-      </Context.Provider>
+      <ErrorBoundary FallbackComponent={()=>Fallback(countError)} onError={onError}>
+         <Context.Provider value={value}>
+            <Header />
+            <Backdrop click={() => setMenuModal(false)} />
+            <main>
+               <Routes>
+                  <Route path="/" element={<Overview />} />
+                  <Route
+                     path="repos"
+                     element={<PaginatedPages itemsPerPage={6} />}
+                  />
+                  {routeDataMap}
+                  <Route path="/error" element={<ErrorCounter />} />
+               </Routes>
+            </main>
+         </Context.Provider>
+      </ErrorBoundary>
    );
 };
 
