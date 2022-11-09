e845148583922975afccd5f2c583b0a722c1b281
babboeCodes <babboecodes@gmail.com>
2022-11-09 01:39:24 +0100

    add menu modal component to header

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -4,15 +4,22 @@ import Image from '../Image/Image';
 import classes from './Header.module.css';
 import Logo from '../../../assets/images/Logo.png';
 import Nav from '../Nav/Nav';
+import MenuModal from '../../Modals/MenuModal/MenuModal';
 
 const Header = () => {
    return (
       <header className={classes.header}>
          <Link to="/">
-            <Image src={Logo}/>
+            <Image src={Logo} />
          </Link>
 
          <Nav />
+         <MenuModal>
+            {/* <Link to="/">
+               <Image src={Logo} maxWidth="10rem"/>
+            </Link> */}
+            <Nav ulClass="flex" />
+         </MenuModal>
       </header>
    );
 };
