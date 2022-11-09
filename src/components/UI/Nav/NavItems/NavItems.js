e6b2e9905f60820803d1914e5ef9c2079537cdae
babboeCodes <babboecodes@gmail.com>
2022-11-09 01:40:42 +0100

    update navItems

--- src/components/UI/Nav/NavItems/NavItems.js
+++ src/components/UI/Nav/NavItems/NavItems.js
@@ -4,24 +4,36 @@ import NavItem from './NavItem/NavItem';
 import overviewIcon from '../../../../assets/icons/overview.svg';
 import repoIcon from '../../../../assets/icons/repo.svg';
 import Context from '../../../Context/Context';
+import DrawerToggle from '../DrawerToggle/DrawerToggle';
 
-const NavItems = () => {
+const NavItems = (props) => {
    const context = useContext(Context);
-   const repoCount = context.userRepos.data
-      ? context.userRepos.data.length
-      : '';
+   const repoCount = context.userRepos.data ? (
+      <p>
+         Repositories{' '}
+         <span className={classes['repo-count']}>
+            {context.userRepos.data.length}
+         </span>
+      </p>
+   ) : (
+      ''
+   );
 
    return (
       <>
-         <ul className={classes['nav-bar__list']}>
+         <ul className={classes[props.ulClass]}>
             <NavItem URL="/">
                <img src={overviewIcon} alt="icon" /> <p>Overview</p>
             </NavItem>
             <NavItem URL="/repos">
                <img src={repoIcon} alt="icon" />{' '}
-               <p>{`Repositories ${repoCount}`}</p>
+               {repoCount}
+            </NavItem>
+            <NavItem URL="/test">
+               <img src={overviewIcon} alt="icon" /> <p>404 Page</p>
             </NavItem>
          </ul>
+         <DrawerToggle />
          <div className={classes['nav-bar__list--after']}></div>
       </>
    );
