a4d2e3fd71c70fdcf76244e6eaa2cbaceb3e5401
babboeCodes <babboecodes@gmail.com>
2022-11-09 01:37:35 +0100

    create Backdrop component

---
+++ src/components/Modals/Backdrop/Backdrop.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import ReactDOM from 'react-dom';
+import Context from '../../Context/Context';
+import classes from './Backdrop.module.css';
+
+const Backdrop = (props) => {
+   const context = useContext(Context);
+   const holder = context.menuModal
+      ? ReactDOM.createPortal(
+           <button className={classes['backdrop']} onClick={props.click}></button>,
+           document.getElementById('backdrop-root'),
+        )
+      : null;
+   return holder;
+};
+
+export default Backdrop;
