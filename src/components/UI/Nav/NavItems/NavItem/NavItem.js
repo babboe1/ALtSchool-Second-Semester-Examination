c952a9e1f2810b62bc5a030e537eeea5e70009e6
babboeCodes <babboecodes@gmail.com>
2022-11-09 01:40:57 +0100

    update Nav item component

--- src/components/UI/Nav/NavItems/NavItem/NavItem.js
+++ src/components/UI/Nav/NavItems/NavItem/NavItem.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import Context from '../../../../Context/Context';
 import classes from '../../../Header/Header.module.css';
 
-const NavItem = (props) => {
+const NavItem = ( props ) => {
+   const context = useContext(Context);
    return (
       <li onMouseOver={props.hover}>
          <NavLink to={props.URL}>
-            <span className={classes['nav-bar__link']}>{props.children}</span>
+            <span className={classes['nav-bar__link']} onClick={context.removeMenuModal}>{props.children}</span>
          </NavLink>
       </li>
    );
