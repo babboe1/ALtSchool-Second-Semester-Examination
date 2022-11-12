161ec651a292c1c04a5642bf9624e16fddd0cc58
babboeCodes <babboecodes@gmail.com>
2022-11-12 12:51:48 +0100

    refactor: minor update

--- src/components/Pages/Repo/Repo.js
+++ src/components/Pages/Repo/Repo.js
@@ -6,14 +6,13 @@ import classes from './Repo.module.css';
 import githubImage from '../../../assets/icons/github.svg';
 import RepoLinkBtn from '../Repositories/RepoTile/RepoLinkBtn/RepoLinkBtn';
 import Context from '../../Context/Context';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Repo = ( props ) => {
-   const nav = useNavigate();
    const context = useContext(Context);
    return (
       <div className={ classes[ 'repositories' ] }>
-         <button className={classes['myButton']} onClick={()=> nav(-1)} >Go Back</button>
+         <Link to="/repos" className={classes['myButton']} >Go Back</Link>
          <RepoTile name={props.name} about={props.about}>
             <ViewRepoBtn
                sty="flip-card__action"
