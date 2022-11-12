8d2d42b5fddccd81d3652e7eba36320af52f9a6c
babboeCodes <babboecodes@gmail.com>
2022-11-12 12:51:40 +0100

    refactor: minor update

--- src/components/Pages/Overview/Overview.js
+++ src/components/Pages/Overview/Overview.js
@@ -7,7 +7,20 @@ import About from './About/About';
 const Overview = (prop) => {
    const context = useContext(Context);
    const userData = context.userData;
-   const loader = context.errorMsg ? <p className={classes.errorMsg}>{context.errorMsg}</p> : <Spinner />;
+   const holder = (
+      <div className={classes.holder}>
+         <p className={classes.errorMsg}>{context.errorMsg}</p>
+         <button
+            className={classes['myButton']}
+            onClick={() => {
+               window.location.reload();
+            }}
+         >
+            Refresh
+         </button>
+      </div>
+   );
+   const loader = context.errorMsg ? holder : <Spinner />;
 
    return (
       <section className={classes.main}>
