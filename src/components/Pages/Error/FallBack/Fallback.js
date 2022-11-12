ddd69b20a8ad5db46ccaa49985f75e998804fb28
babboeCodes <babboecodes@gmail.com>
2022-11-12 12:51:27 +0100

    refactor: minor update

--- src/components/Pages/Error/FallBack/Fallback.js
+++ src/components/Pages/Error/FallBack/Fallback.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import classes from './FallBack.module.css';
 
-const Fallback = (error) => {
+const Fallback = (error, text) => {
+   const navigate = useNavigate();
+   return (
+      <>
+         <div role="alert" className={classes['main']}>
+            <p className={classes['text']}>{text}</p>
+            <p className={classes['errorMsg']}>{error.message}</p>
+            <a
+               href="/"
+               className={classes['myButton']}
+               onClick={() => navigate(-1)}
+            >
+               Go Back Home 😖
+            </a>
+         </div>
+      </>
+   );
+};
+export const Fallbacks = (props) => {
    const navigate = useNavigate();
    return (
       <>
          <div role="alert" className={classes['main']}>
             <p className={classes['text']}>
-               Looks like something went wrong!!!😥
+               {props.text}
             </p>
-            <pre>{error.message}</pre>
-            <a href='/' className={classes['myButton'] } onClick={() => navigate(-1)}>Go Back Home 😖</a>
+            <p className={classes['errorMsg']}>{props.error}</p>
+            <Link to='/' className={classes['myButton'] } onClick={() => navigate(-1)}> {props.send}</Link>
          </div>
       </>
    );
