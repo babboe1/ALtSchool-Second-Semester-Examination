9ebc61be8477c8c806bc9fbb0c8c2e1bc3427d39
babboeCodes <babboecodes@gmail.com>
2022-11-12 12:52:03 +0100

    create Attrinution component

---
+++ src/components/UI/Attribution/Attribution.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Attribution.module.css'
+
+const Attribution = () => {
+   return (
+      <div className={classes['attribution']}>
+         &copy; 2022 All rights reserved. Coded by
+         <a href="https://github.com/babboe1">babboeCodes</a>.
+      </div>
+   );
+};
+
+export default Attribution;
