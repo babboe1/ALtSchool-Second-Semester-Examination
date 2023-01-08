73c22678beb06177a817495bb332a305334f82be
babboeCodes <babboecodes@gmail.com>
2023-01-08 13:17:07 +0100

    run mock test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Attribution from './components/UI/Attribution/Attribution';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('checks attribution name', () => {
+   render(<Attribution />);
+   const titleElement = screen.getByText(/babboeCodes/i);
+   expect(titleElement).toBeInTheDocument();
 });
